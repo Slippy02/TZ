0fd6661db451905137ea51865773df721ae46b21
Slippy01 <t.stepan774@yandex.ru>
2018-02-22 12:19:34 +0400

    class="visible-xs"

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<h1>Hello, world!</h1>
+<h1>??????? 1</h1>
 <?
 $path = $_SERVER['DOCUMENT_ROOT'] . '/img/';
 $images = scandir($path);
@@ -17,9 +17,11 @@ if (false !== $images) {
     $images = preg_grep('/\\.(?:png|gif|jpe?g)$/', $images);
     sort($images, SORT_NUMERIC | SORT_FLAG_CASE);
     foreach ($images as $image)
-        echo '<img height="100" width="100" alt="Where is IMG?" src="/img/', htmlspecialchars(urlencode($image)),'" />';
+        echo '<img height="100" width="100" alt="Where is IMG?" class="visible-xs" src="/img/', htmlspecialchars(urlencode($image)),'" />';
 }
 ?>
+<h1>??????? 2</h1>
+
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
